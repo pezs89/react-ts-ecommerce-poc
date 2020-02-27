df617c7f8d03ce7a0c5409e1f3f2edd3e06a58d2
Zsolt <pezso9@gmail.com>
2020-02-27 09:54:31 +0100

    small refactor in collection overview

--- src/components/CollectionsOverview.tsx
+++ src/components/CollectionsOverview.tsx
@@ -4,22 +4,22 @@ import { createStructuredSelector } from 'reselect';
 
 import CollectionPreview from './CollectionPreview';
 import { ApplicationState } from '../store';
-import { IShopItems } from '../store/features/cart/types';
-import { selectCollections } from '../store/features/shop/selector';
+import { IShopCollection } from '../store/features/cart/types';
+import { selectCollectionsForPreview } from '../store/features/shop/selector';
 
 interface ShopPageSelection {
-  collections: IShopItems
+  collections: Array<IShopCollection>
 }
 
 const mapStateToProps = createStructuredSelector<ApplicationState, ShopPageSelection>({
-  collections: selectCollections
+  collections: selectCollectionsForPreview
 })
 
 type CollectionOverviewProps = ReturnType<typeof mapStateToProps>
 
 const CollectionOverview: React.FC<CollectionOverviewProps> = ({ collections }) => (
   <div className='collections-overview'>
-    {Object.keys(collections).map(key => <CollectionPreview key={collections[key].id} {...collections[key]} />)}
+    {collections.map(collectionItem => <CollectionPreview key={collectionItem.id} {...collectionItem} />)}
   </div>
 )
 
